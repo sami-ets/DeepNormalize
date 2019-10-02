8109a757e3b681c5e4b2eeb09ff4e2bbeb261d13
Pierre-Luc Delisle <github@pierre-luc-delisle.com>
2019-10-02 11:38:09 -0400

    no message

---
+++ deepNormalize/config/experiments/disc_ratio/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+CUDA_VISIBLE_DEVICES=0,1 OMP_NUM_THREADS=1 python3 -m torch.distributed.launch --nproc_per_node=2 ../../../main.py --config=config_disc_ratio_0.00.yaml
+CUDA_VISIBLE_DEVICES=0,1 OMP_NUM_THREADS=1 python3 -m torch.distributed.launch --nproc_per_node=2 ../../../main.py --config=config_disc_ratio_0.15.yaml
+CUDA_VISIBLE_DEVICES=0,1 OMP_NUM_THREADS=1 python3 -m torch.distributed.launch --nproc_per_node=2 ../../../main.py --config=config_disc_ratio_0.25.yaml
+CUDA_VISIBLE_DEVICES=0,1 OMP_NUM_THREADS=1 python3 -m torch.distributed.launch --nproc_per_node=2 ../../../main.py --config=config_disc_ratio_0.50.yaml
+CUDA_VISIBLE_DEVICES=0,1 OMP_NUM_THREADS=1 python3 -m torch.distributed.launch --nproc_per_node=2 ../../../main.py --config=config_disc_ratio_0.75.yaml
+CUDA_VISIBLE_DEVICES=0,1 OMP_NUM_THREADS=1 python3 -m torch.distributed.launch --nproc_per_node=2 ../../../main.py --config=config_disc_ratio_1.00.yaml
