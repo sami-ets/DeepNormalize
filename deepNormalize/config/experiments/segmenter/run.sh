18dd1002af3a0d0b43c4c21a7ae37ba466d310c2
Pierre-Luc Delisle <github@pierre-luc-delisle.com>
2019-10-02 11:33:37 -0400

    Added bash run files.

--- deepNormalize/config/experiments/segmenter/run.sh
+++ deepNormalize/config/experiments/segmenter/run.sh
@@ -8,9 +8,9 @@ CUDA_VISIBLE_DEVICES=0,1 OMP_NUM_THREADS=1 python3 -m torch.distributed.launch -
 CUDA_VISIBLE_DEVICES=0,1 OMP_NUM_THREADS=1 python3 -m torch.distributed.launch --nproc_per_node=2 ../../../main.py --config=config_segmenter_min_max_scaler_MRBrainS.yaml
 
 CUDA_VISIBLE_DEVICES=0,1 OMP_NUM_THREADS=1 python3 -m torch.distributed.launch --nproc_per_node=2 ../../../main.py --config=config_segmenter_quantile_scaler.yaml
-CUDA_VISIBLE_DEVICES=0,1 OMP_NUM_THREADS=1 python3 -m torch.distributed.launch --nproc_per_node=2 ../../../main.py --config=config_segmenter_quantile_scaler_MRBrainS.yaml
 CUDA_VISIBLE_DEVICES=0,1 OMP_NUM_THREADS=1 python3 -m torch.distributed.launch --nproc_per_node=2 ../../../main.py --config=config_segmenter_quantile_scaler_iSEG.yaml
+CUDA_VISIBLE_DEVICES=0,1 OMP_NUM_THREADS=1 python3 -m torch.distributed.launch --nproc_per_node=2 ../../../main.py --config=config_segmenter_quantile_scaler_MRBrainS.yaml
 
 CUDA_VISIBLE_DEVICES=0,1 OMP_NUM_THREADS=1 python3 -m torch.distributed.launch --nproc_per_node=2 ../../../main.py --config=config_segmenter_standardized_scaler.yaml
-CUDA_VISIBLE_DEVICES=0,1 OMP_NUM_THREADS=1 python3 -m torch.distributed.launch --nproc_per_node=2 ../../../main.py --config=config_segmenter_standardized_scaler_MRBrainS.yaml
 CUDA_VISIBLE_DEVICES=0,1 OMP_NUM_THREADS=1 python3 -m torch.distributed.launch --nproc_per_node=2 ../../../main.py --config=config_segmenter_standardized_scaler_iSEG.yaml
+CUDA_VISIBLE_DEVICES=0,1 OMP_NUM_THREADS=1 python3 -m torch.distributed.launch --nproc_per_node=2 ../../../main.py --config=config_segmenter_standardized_scaler_MRBrainS.yaml
