77087f4916dc03bb914ff16eff290e41b393d274
Pierre-Luc Delisle <github@pierre-luc-delisle.com>
2019-10-02 22:58:16 -0400

    no message

--- deepNormalize/config/experiments_school/generator_segmenter/run.sh
+++ deepNormalize/config/experiments_school/generator_segmenter/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-CUDA_VISIBLE_DEVICES=0,1 OMP_NUM_THREADS=1 python3 -m torch.distributed.launch --nproc_per_node=2 ../../../main.py --config=config_generator_segmenter.yaml
-CUDA_VISIBLE_DEVICES=0,1 OMP_NUM_THREADS=1 python3 -m torch.distributed.launch --nproc_per_node=2 ../../../main.py --config=config_generator_segmenter_iSEG.yaml
-CUDA_VISIBLE_DEVICES=0,1 OMP_NUM_THREADS=1 python3 -m torch.distributed.launch --nproc_per_node=2 ../../../main.py --config=config_generator_segmenter_MRBrainS.yaml
+CUDA_VISIBLE_DEVICES=0 OMP_NUM_THREADS=1 python3 -m torch.distributed.launch --nproc_per_node=1 ../../../../main.py --config=config_generator_segmenter.yaml
+CUDA_VISIBLE_DEVICES=0 OMP_NUM_THREADS=1 python3 -m torch.distributed.launch --nproc_per_node=1 ../../../../main.py --config=config_generator_segmenter_iSEG.yaml
+CUDA_VISIBLE_DEVICES=0 OMP_NUM_THREADS=1 python3 -m torch.distributed.launch --nproc_per_node=1 ../../../../main.py --config=config_generator_segmenter_MRBrainS.yaml
