1667fba59a9471d99eae1469876ac2acdffb4127
Pierre-Luc Delisle <github@pierre-luc-delisle.com>
2019-10-09 21:47:58 -0400

    - Upgraded method for deprecation.

--- main.py
+++ main.py
@@ -110,8 +110,8 @@ if __name__ == '__main__':
                                     len(MRBrainS_train) if MRBrainS_train is not None else 0],
                                  y=["iSEG", "MRBrainS"], params={"opts": {"title": "Patch count"}}))
         visdom_logger(VisdomData("Experiment", "Center Voxel Class Count", PlotType.BAR_PLOT, PlotFrequency.EVERY_EPOCH,
-                                 x=[iSEG_CSV.groupby('center_class').count().get_values().flatten() if iSEG_train is not None else 0,
-                                    MRBrainS_CSV.groupby('center_class').count().get_values().flatten() if MRBrainS_train is not None else 0],
+                                 x=[iSEG_CSV.groupby('center_class').count().values().flatten() if iSEG_train is not None else 0,
+                                    MRBrainS_CSV.groupby('center_class').count().values().flatten() if MRBrainS_train is not None else 0],
                                  y=["iSEG", "MRBrainS"], params={"opts": {"title": "Center Voxel Class Count", "stacked": True, "legend":["CSF", "GM", "WM"]}}))
 
         save_folder = "saves/" + os.path.basename(os.path.normpath(visdom_config.env))
