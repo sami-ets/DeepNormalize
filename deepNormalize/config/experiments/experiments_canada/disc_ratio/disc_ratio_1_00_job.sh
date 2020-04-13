c3afbbda7fee07b780324d2f50392260046974b2
Pierre-Luc Delisle <github@pierre-luc-delisle.com>
2020-04-13 11:26:25 -0400

    no message

--- deepNormalize/config/experiments/experiments_canada/disc_ratio/disc_ratio_1_00_job.sh
+++ deepNormalize/config/experiments/experiments_canada/disc_ratio/disc_ratio_1_00_job.sh
@@ -2,7 +2,7 @@
 #SBATCH --account=def-lombaert
 #SBATCH --gres=gpu:1              # Number of GPUs (per node)
 #SBATCH --cpus-per-task=4         # CPU cores/threads
-#SBATCH --mem=64G                 # memory (per node)
+#SBATCH --mem=164G                 # memory (per node)
 #SBATCH --time=03-00:00            # time (DD-HH:MM)
 #SBATCH --mail-user=pierre-luc.delisle@live.com
 #SBATCH --mail-type=BEGIN
