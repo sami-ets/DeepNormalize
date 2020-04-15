3ce4d9d14e09928c8896557171993d4fa02c29a0
Pierre-Luc Delisle <github@pierre-luc-delisle.com>
2020-04-15 17:34:55 -0400

    no message

--- deepNormalize/preprocessing/pipelinev2.py
+++ deepNormalize/preprocessing/pipelinev2.py
@@ -1,15 +1,14 @@
 import argparse
 import logging
 import multiprocessing
-from typing import Union
 
 import abc
 import nibabel as nib
 import numpy as np
 import os
 import re
+from samitorch.inputs.augmentation.transformers import AddBiasField, AddNoise
 from samitorch.inputs.patch import Patch, CenterCoordinate
-from samitorch.inputs.sample import Sample
 from samitorch.inputs.transformers import ToNumpyArray, RemapClassIDs, ToNifti1Image, NiftiToDisk, ApplyMask, \
     ResampleNiftiImageToTemplate, LoadNifti, PadToPatchShape
 from samitorch.utils.files import extract_file_paths
@@ -104,11 +103,14 @@ class iSEGPipeline(AbstractPreProcessingPipeline):
     LOGGER = logging.getLogger("iSEGPipeline")
     PATCH_SIZE = (1, 32, 32, 32)
 
-    def __init__(self, root_dir, output_dir, step, do_extract_patches=True):
+    def __init__(self, root_dir, output_dir, step, do_extract_patches=True, augment=False):
         self._root_dir = root_dir
         self._output_dir = output_dir
         self._step = step
         self._do_extract_patches = do_extract_patches
+        self._augment = augment
+        self._augmentation_transforms = transforms.Compose(
+            [AddBiasField(1.0, alpha=0.5), AddNoise(1.0, snr=60, noise_type="rician")])
 
     def run(self):
         images_T1 = natural_sort(extract_file_paths(os.path.join(self._root_dir, "T1")))
@@ -117,7 +119,7 @@ class iSEGPipeline(AbstractPreProcessingPipeline):
         files = np.stack((np.array(images_T1), np.array(images_T2), np.array(labels)), axis=1)
 
         self._dispatch_jobs(files, 5)
-        # self._do_job(files)
+        #self._do_job(files)
 
     def _do_job(self, files):
         for file in files:
@@ -131,12 +133,16 @@ class iSEGPipeline(AbstractPreProcessingPipeline):
                 self._write_image(label, subject, "Labels")
             self.LOGGER.info("Processing file {}".format(file[0]))
             t1 = self._to_numpy_array(file[0])
+            if self._augment:
+                t1 = self._augmentation_transforms(t1)
             if self._do_extract_patches:
                 self._extract_patches(t1, subject, "T1", self.PATCH_SIZE, self._step)
             else:
                 self._write_image(t1, subject, "T1")
             self.LOGGER.info("Processing file {}".format(file[1]))
             t2 = self._to_numpy_array(file[1])
+            if self._augment:
+                t2 = self._augmentation_transforms(t2)
             if self._do_extract_patches:
                 self._extract_patches(t2, subject, "T2", self.PATCH_SIZE, self._step)
             else:
@@ -181,11 +187,14 @@ class MRBrainSPipeline(AbstractPreProcessingPipeline):
     LOGGER = logging.getLogger("MRBrainSPipeline")
     PATCH_SIZE = (1, 32, 32, 32)
 
-    def __init__(self, root_dir, output_dir, step, do_extract_patches=True):
+    def __init__(self, root_dir, output_dir, step, do_extract_patches=True, augment=False):
         self._root_dir = root_dir
         self._output_dir = output_dir
         self._step = step
         self._do_extract_patches = do_extract_patches
+        self._augment = augment
+        self._augmentation_transforms = transforms.Compose(
+            [AddBiasField(1.0, alpha=0.5), AddNoise(1.0, snr=60, noise_type="rician")])
 
     def run(self):
         source_paths = list()
@@ -226,6 +235,8 @@ class MRBrainSPipeline(AbstractPreProcessingPipeline):
             t1 = t1.transpose((3, 0, 1, 2))
             t1 = np.rot90(t1, axes=(1, -2))
             t1 = self._apply_mask(t1, label_for_testing)
+            if self._augment:
+                t1 = self._augmentation_transforms(t1)
             if self._do_extract_patches:
                 self._extract_patches(t1, subject, "T1", self.PATCH_SIZE, self._step)
             else:
@@ -236,6 +247,8 @@ class MRBrainSPipeline(AbstractPreProcessingPipeline):
             t1_ir = t1_ir.transpose((3, 0, 1, 2))
             t1_ir = np.rot90(t1_ir, axes=(1, -2))
             t1_ir = self._apply_mask(t1_ir, label_for_testing)
+            if self._augment:
+                t1_ir = self._augmentation_transforms(t1_ir)
             if self._do_extract_patches:
                 self._extract_patches(t1_ir, subject, "T1_IR", self.PATCH_SIZE, self._step)
             else:
@@ -246,6 +259,8 @@ class MRBrainSPipeline(AbstractPreProcessingPipeline):
             t2 = t2.transpose((3, 0, 1, 2))
             t2 = np.rot90(t2, axes=(1, -2))
             t2 = self._apply_mask(t2, label_for_testing)
+            if self._augment:
+                t2 = self._augmentation_transforms(t2)
             if self._do_extract_patches:
                 self._extract_patches(t2, subject, "T2_FLAIR", self.PATCH_SIZE, self._step)
             else:
@@ -255,6 +270,8 @@ class MRBrainSPipeline(AbstractPreProcessingPipeline):
             t1_1mm = np.rot90(np.rot90(t1_1mm, axes=(-1, 1)), axes=(1, -2))
             t1_1mm = np.flip(t1_1mm, axis=-1)
             t1_1mm = self._apply_mask(t1_1mm, label_for_testing)
+            if self._augment:
+                t1_1mm = self._augmentation_transforms(t1_1mm)
             if self._do_extract_patches:
                 self._extract_patches(t1_1mm, subject, "T1_1mm", self.PATCH_SIZE, self._step)
             else:
@@ -313,14 +330,15 @@ if __name__ == "__main__":
     #              step=(1, 4, 4, 4)).run()
     # MRBrainSPipeline(args.path_mrbrains, "/data/users/pldelisle/datasets/Preprocessed_4/MRBrainS/DataNii/TrainingData",
     #                  step=(1, 4, 4, 4)).run()
-    #
     # iSEGPipeline(args.path_iseg, "/data/users/pldelisle/datasets/Preprocessed_8/iSEG/Training",
     #              step=(1, 8, 8, 8)).run()
     # MRBrainSPipeline(args.path_mrbrains, "/data/users/pldelisle/datasets/Preprocessed_8/MRBrainS/DataNii/TrainingData",
     #                  step=(1, 8, 8, 8)).run()
-
-    iSEGPipeline(args.path_iseg, "/mnt/md0/Data/Preprocessed/iSEG/Training",
-                 step=None, do_extract_patches=False).run()
-
-    MRBrainSPipeline(args.path_mrbrains, "/mnt/md0/Data/Preprocessed/MRBrainS/DataNii/TrainingData",
-                     step=None, do_extract_patches=False).run()
+    # iSEGPipeline(args.path_iseg, "/mnt/md0/Data/Preprocessed/iSEG/Training",
+    #              step=None, do_extract_patches=False).run()
+    # MRBrainSPipeline(args.path_mrbrains, "/mnt/md0/Data/Preprocessed/MRBrainS/DataNii/TrainingData",
+    #                  step=None, do_extract_patches=False).run()
+    iSEGPipeline(args.path_iseg, "/mnt/md0/Data/Preprocessed_augmented/iSEG/Training",
+                 step=None, do_extract_patches=False, augment=True).run()
+    MRBrainSPipeline(args.path_mrbrains, "/mnt/md0/Data/Preprocessed_augmented/MRBrainS/DataNii/TrainingData",
+                     step=None, do_extract_patches=False, augment=True).run()
