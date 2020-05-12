eb05b844103f8b7226c03d014db5b4aabe6f8e65
Pierre-Luc Delisle <github@pierre-luc-delisle.com>
2020-05-12 14:54:34 -0400

    * Updated ABIDE pipeline for scaled values.

--- deepNormalize/preprocessing/pipelinev2.py
+++ deepNormalize/preprocessing/pipelinev2.py
@@ -1,6 +1,7 @@
 import argparse
 import logging
 import multiprocessing
+from typing import Union
 
 import abc
 import math
@@ -8,10 +9,12 @@ import nibabel as nib
 import numpy as np
 import os
 import re
+from nipype.interfaces import freesurfer
 from samitorch.inputs.augmentation.transformers import AddBiasField, AddNoise
 from samitorch.inputs.patch import Patch, CenterCoordinate
+from samitorch.inputs.sample import Sample
 from samitorch.inputs.transformers import ToNumpyArray, RemapClassIDs, ToNifti1Image, NiftiToDisk, ApplyMask, \
-    ResampleNiftiImageToTemplate, LoadNifti, PadToPatchShape
+    ResampleNiftiImageToTemplate, LoadNifti, PadToPatchShape, CropToContent, PadToShape
 from samitorch.utils.files import extract_file_paths
 from samitorch.utils.slice_builder import SliceBuilder
 from torchvision.transforms import transforms
@@ -379,57 +382,68 @@ class ABIDEPreprocessingPipeline(AbstractPreProcessingPipeline):
         self._do_min_max_scaling = do_min_max_scaling
         self._step = step
         self._normalized_shape = (1, 212, 211, 189)
-        self._transforms = None
-        # self._transform_crop = transforms.Compose([ToNumpyArray(),
-        #                                            CropToContent(),
-        #                                            PadToShape(self._normalized_shape)])
-        # self._transform_align = transforms.Compose([ToNumpyArray(),
-        #                                             Transpose((0, 2, 3, 1)),
-        #                                             Rotate(k=1, axes=(2, 3))
-        #                                             ])
-        self._transform_patch = transforms.Compose([ToNumpyArray(), PadToPatchShape(patch_size=patch_size, step=step)])
+        self._augmentation_transforms = transforms.Compose(
+            [AddBiasField(1.0, alpha=0.5), AddNoise(1.0, snr=60, noise_type="rician")])
 
     def run(self, prefix: str = ""):
         dirs = sorted(next(os.walk(self._root_dir))[1])
         self._dispatch_jobs(dirs, 8)
+        # self._do_job(dirs)
 
     def _do_job(self, dirs):
         for dir in dirs:
+
+            subject = dir
+
             if os.path.exists(os.path.join(self._root_dir, dir, "mri", "aparc+aseg.mgz")):
-                self._extract_labels(os.path.join(self._root_dir, dir, "mri"))
-                self._crop_to_content(os.path.join(self._root_dir, dir, "mri"))
-                self._align(os.path.join(self._root_dir, dir, "mri"))
-                self._apply_mask(os.path.join(self._root_dir, dir, "mri"))
-                self._extract_patches(os.path.join(self._root_dir, dir, "mri"),
-                                      os.path.join(self._root_dir, dir, "mri/patches"), keep_labels=True,
-                                      keep_foreground_only=False)
+                if not os.path.exists(os.path.join(self._output_dir, dir, "mri")):
+                    os.makedirs(os.path.join(self._output_dir, dir, "mri"))
 
-    def _extract_patches(self, root, output_dir, keep_foreground_only=True, keep_labels=True):
-        if not os.path.exists(os.path.join(output_dir, "image")):
-            os.makedirs(os.path.join(output_dir, "image"))
-        if not os.path.exists(os.path.join(output_dir, "labels")):
-            os.makedirs(os.path.join(output_dir, "labels"))
+                self._extract_labels(os.path.join(self._root_dir, dir, "mri"),
+                                     os.path.join(self._output_dir, dir, "mri"))
+                t1, labels = ToNumpyArray()(os.path.join(self._root_dir, dir, "mri", "brainmask.mgz")), ToNumpyArray()(
+                    os.path.join(self._output_dir, dir, "mri", "labels.nii.gz"))
+                t1, labels = self._align(t1, labels)
+                t1, labels = self._crop_to_content(t1, labels)
+                if self._do_min_max_scaling:
+                    t1 = self._min_max_scale(t1)
+                if self._augment:
+                    t1 = self._augmentation_transforms(t1)
+                if self._do_extract_patches:
+                    self._extract_patches(t1, subject, "T1", self._patch_size, self._step)
+                    self._extract_patches(labels, subject, "Labels", self._patch_size, self._step)
+                    os.remove(os.path.join(self._output_dir, subject, "mri", "labels.nii.gz"))
+                else:
+                    self._write_image(t1, subject, "T1")
+                    self._write_image(labels, subject, "Labels")
+                os.remove(os.path.join(self._output_dir, subject, "mri", "csf_mask.mgz"))
+                os.remove(os.path.join(self._output_dir, subject, "mri", "labels.nii.gz"))
+                os.remove(os.path.join(self._output_dir, subject, "mri", "wm_mask.mgz"))
+                os.remove(os.path.join(self._output_dir, subject, "mri", "gm_mask.mgz"))
 
-        transformed_image = Sample(x=os.path.join(root, "real_brainmask.nii.gz"),
-                                   y=os.path.join(root, "aligned_labels.nii.gz"),
-                                   is_labeled=True)
-        self._transform_patch(transformed_image)
+    def _extract_patches(self, image, subject, modality, patch_size, step):
+        transforms_ = transforms.Compose([PadToPatchShape(patch_size=patch_size, step=step)])
+        transformed_image = transforms_(image)
 
-        patches = ABIDEPreprocessingPipeline.get_patches(transformed_image, self._patch_size, self._step,
-                                                         keep_foreground_only)
+        patches = ABIDEPreprocessingPipeline.get_patches(transformed_image, patch_size, step)
 
         for i, patch in enumerate(patches):
             x = transformed_image.x[tuple(patch.slice)]
-            transform_ = transforms.Compose(
-                [ToNifti1Image(), NiftiToDisk(os.path.join(os.path.join(output_dir, "image"), str(i) + ".nii.gz"))])
+            transform_ = transforms.Compose([ToNifti1Image(),
+                                             NiftiToDisk(os.path.join(
+                                                 os.path.join(self._output_dir, subject, "mri", "patches", modality),
+                                                 str(i) + ".nii.gz"))])
             transform_(x)
 
-            if keep_labels:
-                y = transformed_image.y[tuple(patch.slice)]
-                transform_ = transforms.Compose(
-                    [ToNifti1Image(),
-                     NiftiToDisk(os.path.join(os.path.join(output_dir, "labels"), str(i) + ".nii.gz"))])
-                transform_(y)
+    def _write_image(self, image, subject, modality):
+        if not os.path.exists(os.path.join(self._output_dir, subject, modality)):
+            os.makedirs(os.path.join(self._output_dir, subject, "mri", modality))
+
+        transform_ = transforms.Compose(
+            [ToNifti1Image(),
+             NiftiToDisk(os.path.join(self._output_dir, subject, "mri", modality, modality + ".nii.gz"))])
+
+        transform_(image)
 
     @staticmethod
     def get_patches(sample, patch_size, step, keep_foreground_only: bool = True, keep_labels=True):
@@ -449,96 +463,70 @@ class ABIDEPreprocessingPipeline(AbstractPreProcessingPipeline):
         else:
             return patches
 
-    def _apply_mask(self, root):
+    def _apply_mask(self, output_dir):
         try:
-            self._transform_mask = transforms.Compose(
+            transforms.Compose(
                 [LoadNifti(),
-                 ApplyMask(os.path.join(root, "aligned_labels.nii.gz")),
-                 NiftiToDisk(os.path.join(root, "real_brainmask.nii.gz"))])
-            self._transform_mask(os.path.join(root, "aligned_brainmask.nii.gz"))
+                 ApplyMask(os.path.join(output_dir, "labels.nii.gz"))])(os.path.join(output_dir, "brainmask.nii.gz"))
 
         except Exception as e:
             self.LOGGER.warning(e)
 
-    def _align(self, root):
+    def _align(self, t1, label):
         try:
-            sample = Sample(x=os.path.join(root, "cropped_brainmask.nii.gz"),
-                            y=os.path.join(root, "cropped_labels.nii.gz"),
-                            is_labeled=True)
-            transformed_sample = self._transform_align(sample)
+            sample = Sample(x=t1, y=label, is_labeled=True)
+            transformed_sample = transforms.Compose([Transpose((0, 2, 3, 1)),
+                                                     Rotate(k=1, axes=(2, 3)),
+                                                     Rotate(k=2, axes=(1, 3)),
+                                                     ])(sample)
 
-            transforms_ = transforms.Compose([Squeeze(0),
-                                              ToNifti1Image(),
-                                              NiftiToDisk([
-                                                  os.path.join(root, "aligned_brainmask.nii.gz"),
-                                                  os.path.join(root, "aligned_labels.nii.gz")])])
-            transforms_(transformed_sample)
-
+            return transformed_sample.x, transformed_sample.y
         except Exception as e:
             self.LOGGER.warning(e)
 
-    def _extract_labels(self, root):
-        self._mri_binarize(os.path.join(root, "aparc+aseg.mgz"),
-                           os.path.join(root, "wm_mask.mgz"),
+    def _extract_labels(self, input_dir, output_dir):
+        self._mri_binarize(os.path.join(input_dir, "aparc+aseg.mgz"),
+                           os.path.join(output_dir, "wm_mask.mgz"),
                            "wm")
 
-        self._mri_binarize(os.path.join(root, "aparc+aseg.mgz"),
-                           os.path.join(root, "gm_mask.mgz"),
+        self._mri_binarize(os.path.join(input_dir, "aparc+aseg.mgz"),
+                           os.path.join(output_dir, "gm_mask.mgz"),
                            "gm")
-        self._mri_binarize(os.path.join(root, "aparc+aseg.mgz"),
-                           os.path.join(root, "csf_mask.mgz"),
+        self._mri_binarize(os.path.join(input_dir, "aparc+aseg.mgz"),
+                           os.path.join(output_dir, "csf_mask.mgz"),
                            "csf")
 
-        self._remap_labels(os.path.join(root, "gm_mask.mgz"),
-                           os.path.join(root, "remapped_gm_mask.nii.gz"),
-                           1, 2)
-        self._remap_labels(os.path.join(root, "wm_mask.mgz"),
-                           os.path.join(root, "remapped_wm_mask.nii.gz"),
-                           1, 3)
+        csf_labels = ToNumpyArray()(os.path.join(output_dir, "csf_mask.mgz"))
+        gm_labels = self._remap_labels(os.path.join(output_dir, "gm_mask.mgz"), 1, 2)
+        wm_labels = self._remap_labels(os.path.join(output_dir, "wm_mask.mgz"), 1, 3)
 
-        transform_ = transforms.Compose([ToNumpyArray()])
-        wm_vol = transform_(os.path.join(root, "remapped_wm_mask.nii.gz"))
-        gm_vol = transform_(os.path.join(root, "remapped_gm_mask.nii.gz"))
-        csf_vol = transform_(os.path.join(root, "csf_mask.mgz"))
+        merged = self._merge_volumes(gm_labels, wm_labels, csf_labels)
 
-        merged = self._merge_volumes(wm_vol, gm_vol, csf_vol)
+        brainmask = ToNumpyArray()(os.path.join(input_dir, "brainmask.mgz"))
+        T1 = ApplyMask(merged)(brainmask)
+
+        csf = brainmask - T1
+        csf[csf != 0] = 1
+
+        csf_labels = csf_labels + csf
+
+        merged = self._merge_volumes(gm_labels, wm_labels, csf_labels)
 
         transform_ = transforms.Compose(
-            [ToNifti1Image(), NiftiToDisk(os.path.join(root, "labels.nii.gz"))])
+            [ToNifti1Image(), NiftiToDisk(os.path.join(output_dir, "labels.nii.gz"))])
         transform_(merged)
 
-    def _crop_to_content(self, root):
+    def _crop_to_content(self, t1, labels):
+        self._transform_crop = transforms.Compose([CropToContent(),
+                                                   PadToShape(self._normalized_shape)])
         try:
-            sample = Sample(x=os.path.join(root, "brainmask.mgz"),
-                            y=os.path.join(root, "labels.nii.gz"),
-                            is_labeled=True)
+            sample = Sample(x=t1, y=labels, is_labeled=True)
             transformed_image = self._transform_crop(sample)
-            sample = Sample.from_sample(transformed_image)
-            transforms_ = transforms.Compose([Squeeze(0),
-                                              ToNifti1Image(),
-                                              NiftiToDisk([
-                                                  os.path.join(root, "cropped_brainmask.nii.gz"),
-                                                  os.path.join(root, "cropped_labels.nii.gz")])])
-            transforms_(sample)
+
+            return transformed_image.x, transformed_image.y
 
         except Exception as e:
             self.LOGGER.warning(e)
-
-    def _compute_normalized_shape_from_images_in(self, root_dir):
-        image_shapes = []
-
-        for root, dirs, files in os.walk(root_dir):
-            for file in list(filter(lambda path: Image.is_mgz(path) and "brainmask.mgz" in path, files)):
-                try:
-                    self.LOGGER.debug("Computing the bounding box of {}".format(file))
-                    c, d_min, d_max, h_min, h_max, w_min, w_max = CropToContent.extract_content_bounding_box_from(
-                        ToNumpyArray()(os.path.join(root, file)))
-                    image_shapes.append((c, d_max - d_min, h_max - h_min, w_max - w_min))
-                except Exception as e:
-                    self.LOGGER.warning(
-                        "Error while computing the content bounding box for {} with error {}".format(file, e))
-
-        return reduce(lambda a, b: (a[0], max(a[1], b[1]), max(a[2], b[2]), max(a[3], b[3])), image_shapes)
 
     @staticmethod
     def _mri_binarize(input, output, tissue):
@@ -552,16 +540,59 @@ class ABIDEPreprocessingPipeline(AbstractPreProcessingPipeline):
             raise NotImplementedError("Invalid tissue to binarize.")
 
     @staticmethod
-    def _remap_labels(input, output, old_value, remapped_value):
+    def _remap_labels(input, old_value, remapped_value):
         remap_transform = transforms.Compose([ToNumpyArray(),
-                                              RemapClassIDs([old_value], [remapped_value]),
-                                              ToNifti1Image(),
-                                              NiftiToDisk(output)])
+                                              RemapClassIDs([old_value], [remapped_value])])
         return remap_transform(input)
 
     @staticmethod
     def _merge_volumes(volume_1, volume_2, volume_3):
         return volume_1 + volume_2 + volume_3
+
+    @staticmethod
+    def _min_max_scale(file):
+        X = file[0, ...]
+
+        return np.expand_dims(X / X.max(), 0)
+
+
+class Transpose(object):
+    def __init__(self, axis):
+        self._axis = axis
+
+    def __call__(self, input: Union[Sample, np.ndarray]) -> Union[Sample, np.ndarray]:
+        if isinstance(input, np.ndarray):
+            return np.transpose(input, axes=self._axis)
+        elif isinstance(input, Sample):
+            sample = input
+
+            if not isinstance(sample.x, np.ndarray):
+                raise TypeError("Only Numpy arrays are supported.")
+            transformed_sample = Sample.from_sample(sample)
+            transformed_sample.x = np.transpose(sample.x, axes=self._axis)
+            transformed_sample.y = np.transpose(sample.y, axes=self._axis)
+
+            return sample.update(transformed_sample)
+
+
+class Rotate(object):
+    def __init__(self, k, axes):
+        self._k = k
+        self._axes = axes
+
+    def __call__(self, input: np.ndarray):
+        if isinstance(input, np.ndarray):
+            return np.expand_dims(np.rot90(input.squeeze(0), k=self._k), axis=0)
+        elif isinstance(input, Sample):
+            sample = input
+
+            if not isinstance(sample.x, np.ndarray):
+                raise TypeError("Only Numpy arrays are supported.")
+            transformed_sample = Sample.from_sample(sample)
+            transformed_sample.x = np.rot90(sample.x, self._k, self._axes)
+            transformed_sample.y = np.rot90(sample.y, self._k, self._axes)
+
+            return sample.update(transformed_sample)
 
 
 if __name__ == "__main__":
@@ -587,5 +618,6 @@ if __name__ == "__main__":
     #              step=None, do_extract_patches=False, augment=True, do_min_max_scaling=True).run()
     # MRBrainSPipeline(args.path_mrbrains, "/mnt/md0/Data/Preprocessed_scaled_augmented/MRBrainS/DataNii/TrainingData",
     #                  step=None, do_extract_patches=False, augment=True, do_min_max_scaling=True).run()
-    ABIDEPreprocessingPipeline(args.path_abide, "/home/pierre-luc-delisle/ABIDE", step=None, do_extract_patches=False,
+    ABIDEPreprocessingPipeline(args.path_abide, "/home/pierre-luc-delisle/ABIDE_scaled", step=None,
+                               do_extract_patches=False,
                                augment=False, do_min_max_scaling=True).run()
