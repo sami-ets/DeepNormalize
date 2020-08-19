f59913733bd203d72a7f02d4a62b0029828c881d
Pierre-Luc Delisle <pierre-luc.delisle@live.com>
2020-08-19 12:58:38 -0400

    * fixed script

--- eval.py
+++ eval.py
@@ -1,17 +1,19 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn
 import torch
 from kerosene.utils.tensors import to_onehot
 from torch.utils.data import DataLoader
 
 from deepNormalize.inputs.datasets import SingleImageDataset
 from deepNormalize.models.dcgan3d import DCGAN
-from deepNormalize.models.resnet3d import ResNet3D
 from deepNormalize.models.unet3d import Unet
 
 PATCH = 0
 SLICE = 1
 
 PATCH_SIZE = (1, 32, 32, 32)
-STEP = (1, 8, 8, 8)
+STEP = (1, 32, 32, 32)
 
 BATCH_SIZE = 10
 
@@ -19,10 +21,10 @@ if __name__ == "__main__":
     DATA_SET_ROOT_PATH_MR_BRAINS = "/mnt/md0/Data/MRBrainS_scaled/DataNii/TrainingData/"
     DATA_SET_ROOT_PATH_ISEG = "/mnt/md0/Data/iSEG_scaled/Training/"
 
-    # MR_BRAIN_SUBJECT = str(2)
-    # ISEG_SUBJECT = str(9)
-    MR_BRAIN_SUBJECT = str(3)
-    ISEG_SUBJECT = str(6)
+    MR_BRAIN_SUBJECT = str(2)
+    ISEG_SUBJECT = str(9)
+    # MR_BRAIN_SUBJECT = str(3)
+    # ISEG_SUBJECT = str(6)
 
     mr_brains_dataset = SingleImageDataset(root_path=DATA_SET_ROOT_PATH_MR_BRAINS, subject=MR_BRAIN_SUBJECT,
                                            patch_size=PATCH_SIZE, step=STEP)
@@ -41,12 +43,14 @@ if __name__ == "__main__":
     checkpoint = torch.load("/mnt/md0/models/Generator/Generator.tar")
     generator.load_state_dict(checkpoint["model_state_dict"])
     generator.cuda()
-    checkpoint = torch.load("/mnt/md0/models/Discriminator/Discriminator2.tar")
+    checkpoint = torch.load("/mnt/md0/models/Discriminator/Discriminator.tar")
     discriminator.load_state_dict(checkpoint["model_state_dict"])
     discriminator.cuda()
 
     iseg_pred = torch.zeros(3, ).cuda()
-    mr_brain_pred = torch.zeros(3, ).cuda()
+    iseg_pred_real = torch.zeros(3, ).cuda()
+    mrbrains_pred = torch.zeros(3, ).cuda()
+    mrbrains_pred_real = torch.zeros(3, ).cuda()
 
     iseg_count = 0
     mrbrain_count = 0
@@ -56,8 +60,13 @@ if __name__ == "__main__":
 
         disc_pred = discriminator.forward(normalised_patches)[0]
 
-        mr_brain_pred = mr_brain_pred + to_onehot(torch.argmax(torch.softmax(disc_pred, dim=1), dim=1),
+        mrbrains_pred = mrbrains_pred + to_onehot(torch.argmax(torch.softmax(disc_pred, dim=1), dim=1),
                                                   num_classes=3).sum(dim=0)
+
+        disc_pred = discriminator.forward(inputs.cuda())[0]
+
+        mrbrains_pred_real += to_onehot(torch.argmax(torch.softmax(disc_pred, dim=1), dim=1),
+                                        num_classes=3).sum(dim=0)
 
         mrbrain_count += disc_pred.shape[0]
 
@@ -69,6 +78,18 @@ if __name__ == "__main__":
         iseg_pred = iseg_pred + to_onehot(torch.argmax(torch.softmax(disc_pred, dim=1), dim=1),
                                           num_classes=3).sum(dim=0)
 
+        disc_pred = discriminator.forward(inputs.cuda())[0]
+
+        iseg_pred_real += to_onehot(torch.argmax(torch.softmax(disc_pred, dim=1), dim=1),
+                                    num_classes=3).sum(dim=0)
+
         iseg_count += disc_pred.shape[0]
 
-    print(str(iseg_pred.cpu().numpy() / iseg_count), str(mr_brain_pred.cpu().numpy() / mrbrain_count))
+    print(str(iseg_pred.cpu().numpy() / iseg_count), str(mrbrains_pred.cpu().numpy() / mrbrain_count))
+    print(str(iseg_pred_real.cpu().numpy() / iseg_count), str(mrbrains_pred_real.cpu().numpy() / mrbrain_count))
+
+    data = (np.vstack((mrbrains_pred.cpu().numpy()/mrbrains_pred.sum().cpu().numpy(), iseg_pred.cpu().numpy()/iseg_pred.sum().cpu().numpy(), mrbrains_pred_real.cpu().numpy()/mrbrains_pred_real.sum().cpu().numpy(),
+                      iseg_pred_real.cpu().numpy()/iseg_pred_real.sum().cpu().numpy())))
+           # / (mrbrains_pred+iseg_pred+mrbrains_pred_real+iseg_pred_real).sum().cpu().numpy()
+    sns = seaborn.heatmap(data, annot=True, yticklabels=["Gen MRBrainS", "Gen iSEG", "MRBrainS", "iSEG"], xticklabels=["Generated", "MRBRainS", "iSEG"], cmap="RdBu_r")
+    plt.show()
